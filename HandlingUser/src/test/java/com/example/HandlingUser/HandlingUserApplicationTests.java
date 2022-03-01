d677acc8bcad17a9fc239b3512cb46c586c93281
niranjana <67066151+niranjana-kamalapuram@users.noreply.github.com>
2022-03-01 16:19:20 +0530

    Add files via upload

---
+++ HandlingUser/src/test/java/com/example/HandlingUser/HandlingUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HandlingUser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandlingUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
